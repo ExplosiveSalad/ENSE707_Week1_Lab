e7e9be8bff0680e7d570555f64de6efbee91fdfb
Mohammed Yaacoub Abou Chlih <kbq5169@autuni.ac.nz>
2026-07-16 11:14:51 +1200

    Finished Activities 1 2 and 3

---
+++ ENSE707_Week1_Lab/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENSE707_Week1_Lab
+{
+    public class BankAccount
+    {
+        public string AccountHolder { get; set; }
+        public decimal Balance { get; private set; }
+        public BankAccount(string accountHolder, decimal openingBalance)
+        {
+            AccountHolder = accountHolder;
+            Balance = openingBalance;
+        }
+        public void Deposit(decimal amount)
+        {
+            Balance = Balance + amount;
+            if (amount < 0)
+            {
+                throw new InvalidOperationException("Deposit amount cannot be negative.");
+            }
+        }
+        public bool Withdraw(decimal amount)
+        {
+            Balance = Balance - amount;
+            return true;
+        }
+        public decimal CalculateTransationFee(decimal amount)
+        {
+            return amount * 0.02m;
+        }
+    }
+}
--- ENSE707_Week1_Lab/Program.cs
+++ ENSE707_Week1_Lab/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Test");
+﻿using ENSE707_Week1_Lab;
+
+BankAccount account = new BankAccount("Student User", 100);
+
+account.Deposit(50);
+account.Withdraw(30);
+
+Console.WriteLine($"Account Holder: {account.AccountHolder}");
+Console.WriteLine($"Current Balance: {account.Balance}");
+Console.WriteLine($"Fee on $100 transaction: {account.CalculateTransationFee(100)}");
